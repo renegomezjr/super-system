23d4ce7ddadab1ea47ae53f1bb5940c0d0682b6c
Rene Gomez <renegomezjr@gmail.com>
2016-06-01 13:48:32 -0400

    Initial commit

---
+++ ExerciseConsoleApp/ExerciseConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.Write("How long would you like to exercise?");
+        }
+    }
+}
