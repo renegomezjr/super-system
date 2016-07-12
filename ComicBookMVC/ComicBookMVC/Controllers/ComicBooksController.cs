a7323aa5da1acad6a1e1cb14688f18948ed7346f
Rene Gomez <renegomezjr@gmail.com>
2016-07-12 15:23:17 -0400

    Updated controller with Action Result.

---
+++ ComicBookMVC/ComicBookMVC/Controllers/ComicBooksController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookMVC.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public ActionResult Detail()
+        {
+            if(DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
+            {
+                return Redirect("/");
+            }
+            return Content("hello");
+            
+        }
+    }
+}
